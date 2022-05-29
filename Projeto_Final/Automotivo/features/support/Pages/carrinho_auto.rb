9db47472d6554a6b6940ecf3733387242d02a6f6
Henrique <henrique.reis@webjump.com.br>
2022-05-29 19:56:03 -0300

    Alterações nos cenários

--- Projeto_Final/Automotivo/features/support/Pages/carrinho_auto.rb
+++ Projeto_Final/Automotivo/features/support/Pages/carrinho_auto.rb
@@ -1,17 +1,9 @@
 class Shops < Methods
 include Capybara::DSL
-    
-    def Login(p_email, p_senha)
-        click_link 'Entre'
-        fillFields(EL['emailLogin'], p_email)
-        fillFields(EL['passwordLogin'], p_senha)
-        click_button 'Entre'
-    end
-    
+        
     def Product1 
         first(".product-image-photo").click()
-        #binding.pry
-        find("option[value='#{5}']").select_option
+        find("option[value='#{6}']").select_option
         click_button 'Adicionar ao Carrinho'
     end
     
